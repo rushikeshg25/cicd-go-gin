88a69ec4ec9bb9e9bcbfbeffcbd82e1796447e95
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2025-05-03 16:23:29 +0530

    feat: basic GET and POST

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+type Input struct {
+	Name string `json:"name" binding:"required"`
+}
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World!",
+		})
+	})
+
+	r.POST("/hello-post", func(ctx *gin.Context) {
+		var input Input
+		if err := ctx.ShouldBindJSON(&input); err != nil {
+			ctx.JSON(400, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		ctx.JSON(200, gin.H{
+			"message": input.Name,
+		})
+	})
+
+	r.Run(":8080")
+}
